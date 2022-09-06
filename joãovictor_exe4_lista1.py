3ddabae4c76334ae1f13f780cb5d2274e94169d1
João Victor Soares da Silva <105072791+juaovitu13@users.noreply.github.com>
2022-09-06 11:16:25 -0300

    Add files via upload

---
+++ joãovictor_exe4_lista1.py
@@ -0,0 +1,11 @@
+#tempo_do_evento
+
+x = int(input("Quantidade de segundos:"))
+
+#calculo
+
+horas = x / 3600
+minutos = x / 60
+segundos = x 
+
+print("A quantidade em Horas:",horas,"em minutos:",minutos,"em segundos:",segundos)
