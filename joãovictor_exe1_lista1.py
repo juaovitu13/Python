97d9bc62b28ceacb9010beeff968b25037f124da
João Victor Soares da Silva <105072791+juaovitu13@users.noreply.github.com>
2022-09-06 11:11:58 -0300

    Add files via upload
    Exercicio basico de uma lista de exercicios.

---
+++ joãovictor_exe1_lista1.py
@@ -0,0 +1,10 @@
+#velocidade_media
+
+distancia = float(input("Coloque a distância:"))
+velocidade = float(input("Coloque a velocidade:"))
+
+#Execução_do_calculo
+
+tempo = distancia / velocidade
+
+print("Seu tempo de viagem é: ",tempo)
